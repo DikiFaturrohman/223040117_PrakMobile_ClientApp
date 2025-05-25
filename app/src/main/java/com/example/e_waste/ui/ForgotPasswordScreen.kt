b096f52060c8c97481b3fcbb5e5863f5710f709b
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2025-05-25 19:32:32 +0700

    refactor : menghapus import yang tidak perlu

--- app/src/main/java/com/example/e_waste/ui/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/e_waste/ui/ForgotPasswordScreen.kt
@@ -1,35 +1,20 @@
 package com.example.e_waste.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.e_waste.ui.viewmodels.AuthViewModel
-import java.text.SimpleDateFormat
-import java.util.*
 
 // ForgotPasswordScreen.kt
 @Composable
