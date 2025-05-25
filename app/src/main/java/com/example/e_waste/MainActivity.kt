3c320627cb0efc24c38cef2d3f54480c8b2486a6
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2025-05-25 19:27:29 +0700

    feat : menambah kelas app

--- app/src/main/java/com/example/e_waste/MainActivity.kt
+++ app/src/main/java/com/example/e_waste/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EWasteAppTheme {
+            EwasteTheme {
                 EWasteApp()
             }
         }
